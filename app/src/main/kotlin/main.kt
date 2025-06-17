af696d0a6e7a2343f155d530c0cc7f370b3e9f44
DanielWestgaard <daniel.westgaard@gmail.com>
2025-06-17 16:50:30 +0200

    finally got it working in intellij idea (ce)

---
+++ app/src/main/kotlin/main.kt
@@ -0,0 +1,6 @@
+fun main(){
+    println("Hello asldfjhalskdfhjlak!")
+    var sum = 0
+    println("12 + 12")
+    println("testing")
+}
--- build.gradle.kts
+++
@@ -1,9 +0,0 @@
-dependencies {
-    implementation("io.ktor:ktor-server-core:2.3.4")
-    implementation("io.ktor:ktor-server-netty:2.3.4")
-    implementation("io.ktor:ktor-server-auth:2.3.4")
-    implementation("io.ktor:ktor-server-auth-jwt:2.3.4")
-    implementation("io.ktor:ktor-server-content-negotiation:2.3.4")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.4")
-    implementation("com.nimbusds:nimbus-jose-jwt:9.38")
-}
